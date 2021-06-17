26fb2d726dac04daecab4b29d3a1109ae82fc41c
skyheat47295 <70936076+skyheat47295@users.noreply.github.com>
2021-06-17 15:32:11 -0700

    6.17.21

--- PrimeGov_scraper/google_drive_uploader.py
+++ PrimeGov_scraper/google_drive_uploader.py
@@ -6,7 +6,8 @@ import os
 Due to the nature of Google drive files, duplicate files will not be over-written but instead will be duplicated.
 """
 
-local_path = './test.data'  # Path to local files to be uploaded.
+local_path = './data'  # Path to local files to be uploaded.
+
 # Path to remote G-Drive directory to upload files. Special note. this is a tag not a directory
 g_path = 'PrimeGov.data'
 file_list = os.listdir(local_path)
@@ -18,7 +19,7 @@ gauth.LoadCredentialsFile('mycreds.txt')
 gauth.LocalWebserverAuth()
 drive = GoogleDrive(gauth)
 
-folder = drive.ListFile({'q': f"title = '{g_path}' and trashed=false"}).GetList()[0]  # get the folder we just created
+folder = drive.ListFile({'q': f"title = '{g_path}' and trashed=false"}).GetList()[0]  # find ID for g_path folder
 print('Uploading file(s)')
 for file in file_list:
     g_file = drive.CreateFile({'title': file, 'parents': [{'id': folder['id']}]})
